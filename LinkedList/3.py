9f005145fc868a0cfc2c14e6185fb47bdda0f7b7
akhoa6204 <khoaanh662004@gmail.com>
2023-10-18 00:24:42 +0700

    sád

--- LinkedList/3.py
+++ LinkedList/3.py
@@ -7,60 +7,58 @@ class Book:
         self.title=Title
         self.author=Author
         self.isbn=ISBN
-class Main: 
+class Main : 
     def __init__(self):
-        self.head=None
-        self.last=None
-    def Print(self): 
-        cur=self.head
-        while cur != None  : 
+        self.head=None 
+        self.last=None 
+    def Add(self,title,author,isbn): 
+        newbook=Book(title,author,isbn)
+        book=node(newbook)
+        if self.head is None : 
+            self.head=book 
+            self.last=book
+        else: 
+            self.last.next  = book
+            self.last=book 
+    def Print(self):
+        cur = self.head
+        while cur is not  None :
             print(cur.data.title,cur.data.author,cur.data.isbn,end=' ')
-            if cur != self.last:
+            if cur != self.last :
                 print('|',end=' ')
-            cur=cur.next
+            cur = cur.next
         print()
-    def Add(self,name): 
-        name=node(name)
-        if self.head is not None: 
-            self.last.next=name 
-            self.last=name 
-        else : 
-            self.head=name
-            self.last=name
     def Remove(self,name):
         cur=self.head
-        pre =None 
-        while cur.data !=name and self.head is not None: 
-            pre=cur 
-            cur=cur.next 
-        if cur== self.head: 
-            self.head=cur.next 
+        pre = None 
+        while cur.data.title != name and self.head!= None : 
+            pre =cur 
+            cur = cur.next 
+        if cur == self.head : 
+            self.head=cur.next
             cur.next=None 
-        elif cur== self.last: 
-            self.last= pre
-            pre.next=None 
+        elif cur== self.last : 
+            self.last= pre 
+            pre.next= None 
         else: 
-            pre.next= cur.next
-            cur.next= None 
-        del cur
+            pre.next=cur.next 
+            cur.next = None 
+        del cur 
     def Find(self,name):
-        i=0
         cur=self.head
-        while cur.data !=name and self.head is not None: 
-            cur=cur.next
-            i+=1
-        print('Số thứ tự',i)
-book1=Book('toan','khoa','123')  
-book2=Book('hoa','ngoc','456')
-book3=Book('ly','bo','789')
-main=Main()
-main.Add(book1)
-main.Add(book2)
-main.Add(book3)
-main.Print()
-main.Remove(book1)
-main.Print()
-main.Find(book3)
+        i=1
+        while cur.data.title != name and self.head!= None : 
+            cur =cur.next
+            i+=1 
+        print('Số thứ tự:',i)
 
-  
+main=Main()
+main.Add(input('Title: '),input('Author: '),int(input('ISBN: ')))
+main.Add(input('Title: '),input('Author: '),int(input('ISBN: ')))
+main.Add(input('Title: '),input('Author: '),int(input('ISBN: ')))
+main.Print()
+main.Remove(input('Title cần xóa: '))
+main.Print()
+main.Find(input('Title cần tìm: '))
+
                     
